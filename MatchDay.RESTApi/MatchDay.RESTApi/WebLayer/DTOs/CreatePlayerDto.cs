4a8e1e75ac36e766be5de7d4c40236e8dcb2e0ec
Alexis Saari <alexissaari@gmail.com>
2025-07-08 11:43:52 -0400

    validation

--- MatchDay.RESTApi/MatchDay.RESTApi/WebLayer/DTOs/CreatePlayerDto.cs
+++ MatchDay.RESTApi/MatchDay.RESTApi/WebLayer/DTOs/CreatePlayerDto.cs
@@ -2,7 +2,7 @@
 {
     public class CreatePlayerDto
     {
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
---
+++ MatchDay.RESTApi/MatchDay.RESTApi/WebLayer/Validators/CreateCoachDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MatchDay.RESTApi.WebLayer.DTOs;
+
+namespace MatchDay.RESTApi.WebLayer.Validators
+{
+    public class CreateCoachDtoValidator : AbstractValidator<CreateCoachDto>
+    {
+        public CreateCoachDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("Coach's first name is required.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Coach's last name is required.");
+        }
+    }
+}
